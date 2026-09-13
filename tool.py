ea045b8818eacd28e91e38fc1e63bebf1be8215c
discorduidbot-hue <discorduidbot@gmail.com>
2026-09-13 04:45:08 -0700

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,4 +1,4 @@
- import os
+import os
 import discord
 import asyncio
 
@@ -221,7 +221,7 @@ async def panel(ctx):
         title="<a:welcome:1533417041615392879>  **AXB STORE**   <a:welcome:1533417041615392879> ",
         description=(
             "━━━━━━━━━━━━━━━━━━━━━━\n"
-            "🛒  PURCHASE CENTER\n"
+            "<a:welcome:1546092136787611658> PURCHASE CENTER\n"
             "━━━━━━━━━━━━━━━━━━━━━━\n\n"
             "Select Your Product From The Menu Below.\n\n"
             "<a:welcome:1533400305775939636> PREMIUM UID BYPASS\n"
